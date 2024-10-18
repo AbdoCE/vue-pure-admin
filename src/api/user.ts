c1bfd41fd4247c3edcf9c8b0e01f06a0618ee53e
AbdoCE <AbdoCE1@outlook.com>
2024-10-18 23:27:08 +0700

    a

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,26 +1,23 @@
 import { http } from "@/utils/http";
 
 export type UserResult = {
-  err: boolean;
-  msg: string;
-  data: {
-    /** 头像 */
-    avatar: string;
-    /** 用户名 */
+  success: boolean;
+  message: string;
+  accessToken: string;
+  userInfo: {
+    user_id: number;
+    user_name: string;
+    user_password: string;
+    user_fullname: string;
+    user_token: string;
+    user_avatar: string;
     user_email: string;
-    /** 昵称 */
-    nickname: string;
-    /** 当前登录用户的角色 */
-    roles: Array<string>;
-    /** 按钮级别权限 */
-    permissions: Array<string>;
-    /** `token` */
-    accessToken: string;
-    /** 用于调用刷新`accessToken`的接口时所需的`token` */
-    refreshToken: string;
-    /** `accessToken`的过期时间（格式'xxxx/xx/xx xx:xx:xx'） */
-    expires: Date;
+    user_role: string;
+    user_key: string;
+    user_logs: string;
+    user_loginid: string;
   };
+  user_loginid: string;
 };
 
 export type RefreshTokenResult = {
--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -81,7 +81,7 @@ export const useUserStore = defineStore({
       return new Promise<UserResult>((resolve, reject) => {
         getLogin(data)
           .then(data => {
-            if (data?.err) setToken(data.data);
+            if (data?.success) setToken(data.userInfo);
             resolve(data);
           })
           .catch(error => {
